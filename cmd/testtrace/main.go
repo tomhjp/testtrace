dd9525118f2cb13a3b1e0b3a7370899ae06c0ee2
Tom Proctor <tomhjp@users.noreply.github.com>
2026-07-31 12:59:07 +0100

    cmd/testtrace: add -i and -o flags for flexible in/out
    Read from stdin if we're being piped to or receiving a file redirection,
    and otherwise open the file specified by the -i flag.
    
    Write to stdout, or a file path specified by -o if provided.

--- cmd/testtrace/main.go
+++ cmd/testtrace/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
-	"log"
+	"errors"
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/tomhjp/testtrace"
@@ -10,16 +12,56 @@ import (
 
 func main() {
 	if err := run(); err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
 func run() error {
-	tw, err := testtrace.NewTraceWriter(os.Stdout)
+	var (
+		in  = flag.String("i", "", "path to a file with recorded go test -json output; use - for stdin")
+		out = flag.String("o", "", "path to write the trace to; defaults to stdout")
+	)
+	flag.Parse()
+
+	w := os.Stdout
+	if *out != "" {
+		wf, err := os.OpenFile(*out, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o644)
+		if err != nil {
+			return fmt.Errorf("error opening out file %q: %w", *out, err)
+		}
+		defer wf.Close()
+		w = wf
+	}
+	tw, err := testtrace.NewTraceWriter(w)
 	if err != nil {
 		return err
 	}
-	s := bufio.NewScanner(os.Stdin)
+
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		return err
+	}
+
+	var s *bufio.Scanner
+	if (info.Mode()&os.ModeNamedPipe) != 0 || (info.Mode()&os.ModeType) == 0 {
+		// Either a pipe or file redirection is attached to stdin, read that.
+		s = bufio.NewScanner(os.Stdin)
+	} else {
+		switch *in {
+		case "":
+			// TODO: support running the tests ourselves
+			return errors.New("must pipe go test -json output to stdin or pass -i")
+		case "-":
+			s = bufio.NewScanner(os.Stdin)
+		default:
+			rf, err := os.Open(*in)
+			if err != nil {
+				return fmt.Errorf("error opening %q: %w", *in, err)
+			}
+			defer rf.Close()
+			s = bufio.NewScanner(rf)
+		}
+	}
 	for s.Scan() {
 		if err := tw.AddTest2JSONLine(s.Bytes()); err != nil {
 			return err
